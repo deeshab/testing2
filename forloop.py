4d348994e6b625c8ed46f702dc1042ed50b51b51
Deesha Bhaumik <deeshabhaumik@Deeshas-MacBook-Pro.local>
2018-09-05 22:36:31 -0400

    added forloop.py on sept 5

---
+++ forloop.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#rzhang26:20171027:HW7A:forloop.py
+
+#Q2a
+s0 = 'computing'
+s1 = 'introduce'
+d0 = {'a ':3,'b ':5,'c ':2}
+
+#Q2b
+for i in s0:
+	print(i)
+
+#Q2c
+for i in range(len(s0)):
+	print(i, s0[i])
+
+#Q2d
+for i in range(len(s1)):
+	print(s1[i] + " " + s0[i])
+
+for i in d0.keys():
+	j = d0[i]
+	l = ""
+	for k in range(j):
+		l = l + i	
+	print(l)
